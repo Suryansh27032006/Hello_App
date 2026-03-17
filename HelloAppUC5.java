a8d5481791b89063c232dcc51197e20e236252f2
Suryansh27032006 <us8457@srmist.edu.in>
2026-03-17 14:12:04 +0530

    HelloAppUC5.java

---
+++ HelloAppUC5.java
@@ -0,0 +1,20 @@
+public class HelloAppUC5 {
+    public static void main(String[] args) {
+
+        StringBuilder names = new StringBuilder();
+
+        if (args.length == 0) {
+            names.append("World");
+        } else {
+            // Enhanced for loop
+            for (String name : args) {
+                names.append(name).append(", ");
+            }
+
+            // Remove last comma and space
+            names.setLength(names.length() - 2);
+        }
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
